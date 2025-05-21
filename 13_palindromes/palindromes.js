a954286faecc15b131c3db3bd2e261cd5aff9e83
chadriley722 <chadriley722@gmail.com>
2025-05-21 13:29:10 -0600

    Finished 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  // Convert to lowercase and remove all non-alphanumeric characters
+  const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+  
+  // Compare the string with its reverse
+  const reversedString = processedString.split('').reverse().join('');
+  
+  return processedString === reversedString;
 };
 
 // Do not edit below this line

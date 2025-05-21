d167834c281d721de29c44c1a0d7aff7b19acc53
chadriley722 <chadriley722@gmail.com>
2025-05-21 13:34:52 -0600

    Finished 16_findTheOldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    
+    return people.reduce((oldest, current) => {
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        const currentAge = (current.yearOfDeath || currentYear) - current.yearOfBirth;
+        
+        return currentAge > oldestAge ? current : oldest;
+    });
 };
 
 // Do not edit below this line

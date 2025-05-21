9dc821ba86b8d3c6207b9e2110bd95c2d402a22d
chadriley722 <chadriley722@gmail.com>
2025-05-21 09:50:25 -0600

    Finished 09_sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+  // Check if inputs are positive integers
+  if (typeof start !== 'number' || typeof end !== 'number' || 
+      start < 0 || end < 0 || 
+      !Number.isInteger(start) || !Number.isInteger(end)) {
+    return 'ERROR';
+  }
+  
+  // Ensure start is smaller than end
+  if (start > end) {
+    [start, end] = [end, start];
+  }
+  
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  
+  return sum;
 };
 
 // Do not edit below this line

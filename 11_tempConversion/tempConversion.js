39abc3a45e9ee9a4e90c327acc9af4708dc1b279
chadriley722 <chadriley722@gmail.com>
2025-05-21 09:58:54 -0600

    Finished 11_tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // Formula: (F - 32) * 5/9
+  const celsius = (fahrenheit - 32) * (5 / 9);
+  // Round to 1 decimal place
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // Formula: (C * 9/5) + 32
+  const fahrenheit = (celsius * 9 / 5) + 32;
+  // Round to 1 decimal place
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
